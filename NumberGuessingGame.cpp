a64606a9c9c4894cc9cbac6a08fa0efc72b64442
Amar Zilic <amardabest375@gmail.com>
2024-11-19 16:34:13 +0100

    Now tells you what the correct number was when you entered the wrong number

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 
 
@@ -11,45 +12,54 @@ int RandomNumber(int min, int max){
 
 int main(int argc, char const *argv[])
 {
-    int correctGuessed = 0;
-    cout << "Type in a number, to guess another number between it! :";
-    int play;
-    cin >> play;
+    while (true) {
+        int correctGuessed = 0;
+        cout << "Type in a number, to guess another number between 1 and x! : ";
+        int play;
+        cin >> play;
 
-    // terminate on error
-    if (cin.fail()){
-        terminate();
-        return -1;
-    }
-
-    if (play < 0) {
-        cout << "You can't have negative numbers!";
-        terminate();
-        return -1;
-    }
-
-    while (true){
-        cout << "Type in a number from 1-" << play << " and try to guess it! (Enter any character to quit)" << endl;
-        int value;
-        cin >> value;
-
-        int randNum = RandomNumber(1,play);
+        // terminate on error
         if (cin.fail()){
-            cout << "Invalid input!" << endl;
             terminate();
             return -1;
         }
-        if (value == randNum){
-            correctGuessed++;
-            cout << "Successfully guessed number! Answered " << correctGuessed << " correctly so far!" << endl;
-        }else if (value > play){
-            cout << "Your number is too big!!" << endl;
-        }else{
-            cout << "Whoops! Looks like you failed.." << endl;
-            correctGuessed = 0;
+
+        // terminate if its a negative number
+        if (play < 0) {
+            cout << "You can't have negative numbers!";
+            terminate();
+            return -1;
         }
-        
-        
+
+        while (true){
+            cout << "Type in a number from 1-" << play << " and try to guess it! (Enter any character to quit or enter your amount + 1 to go back to the range setting)" << endl;
+            int value;
+            cin >> value;
+
+            int randNum = RandomNumber(1,play);
+            if (cin.fail()){
+                cout << "Invalid input!" << endl;
+                terminate();
+                return -1;
+            }
+            if (value == randNum){
+                // correctly guessed
+                correctGuessed++;
+                cout << "Successfully guessed number! Answered " << correctGuessed << " correctly so far!" << endl;
+            }else if (value == play + 1){
+                // go back to the setting
+                break;
+            }else if (value > play){
+                // number too big
+                cout << "Your number is too big!!" << endl;
+                cout << "The answer was " << randNum << endl;
+            }else{
+                // guessed incorrectly
+                cout << "Whoops! Looks like you failed.." << endl;
+                correctGuessed = 0;
+                cout << "The answer was " << randNum << endl;
+            }
+        }
     }
     return 0;
 }
